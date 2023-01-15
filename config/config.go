1814db6a9b4cad5a6413a922637a5ca6d84d3ec6
Oyakodon <oykdn@oykdn.com>
2023-01-15 20:29:36 +0900

    フォント設定を追加

--- config/config.go
+++ config/config.go
@@ -6,6 +6,12 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+type BotConfigFont struct {
+	Id   string `yaml:"id"`
+	Name string `yaml:"name"`
+	Path string `yaml:"path"`
+}
+
 type BotConfig struct {
 	BotToken string `yaml:"bottoken"`
 
@@ -13,8 +19,9 @@ type BotConfig struct {
 	Channels []string `yaml:"channels"`
 
 	NoticeChannelId string `yaml:"noticechannelid"`
+	Nickname        string `yaml:"nickname"`
 
-	Nickname string `yaml:"nickname"`
+	Fonts []BotConfigFont `yaml:"fonts"`
 }
 
 func LoadConfig(path string) (*BotConfig, error) {
